b9be2bdfbb9ac418b9eeb5a354a29dd47f54676f
Vagrant <Vagrant>
2017-12-08 13:24:17 -0800

    port

--- index.js
+++ index.js
@@ -117,6 +117,6 @@ bot.on('message', (payload, reply) => {
     })
   })
 })
-
-http.createServer(bot.middleware()).listen(3000)
-console.log('Echo bot server running at port 3000.')
+var port = process.env.PORT || 3000;
+http.createServer(bot.middleware()).listen(port)
+console.log('Echo bot server running at port.')
